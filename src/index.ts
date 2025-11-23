cd12bc619d3e49f5a6fa946597699c45acc7e564
madonuko <madonuko@outlook.com>
2025-11-24 02:08:35 +0800

    feat: basic things ready

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import fs from "fs";
+import path from "path";
+
+export type I18NyaConfig = {
+  /** Path to directory containing language files */
+  langDir: string;
+  /** Locale ID for default language */
+  defaultLang: string;
+  fallbackLangs?: Record<string, string>;
+};
+
+export type Interpolations = Record<string, string | { toString(): string }>;
+
+export const init = async ({
+  langDir,
+  defaultLang: rootLang,
+  fallbackLangs: fb = {},
+}: I18NyaConfig) => {
+  const DefaultTranslations = await import(`${langDir}/${rootLang}.json`);
+  type TransKeys = keyof typeof DefaultTranslations;
+  type I18Nya = {
+    translations: Record<string, Record<TransKeys, string>>;
+    makeT: (lang: string) => (key: TransKeys, its?: Interpolations) => string;
+  };
+  let i18nya: I18Nya = {
+    translations: {},
+    makeT:
+      (l: string) =>
+      (k: TransKeys, its: Interpolations = {}) => {
+        let s: string | undefined;
+        for (; !(s = i18nya.translations[l]?.[k]); l = fb[l] ?? rootLang) {
+          if (l === rootLang) {
+            return String(k);
+          }
+        }
+        for (const [k, v] of Object.entries(its)) {
+          s = s.replace(`{{${k}}}`, `${v}`);
+        }
+        return s;
+      },
+  };
+  for (const entry of fs.readdirSync(langDir, { withFileTypes: true })) {
+    if (entry.isFile() && entry.name.endsWith(".json")) {
+      const lang = entry.name.slice(0, -5);
+      i18nya.translations[lang] = await import(path.join(langDir, entry.name));
+    }
+  }
+  return i18nya;
+};
